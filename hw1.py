6f04fbd504b3b388784e1af33a22d98cf2647f95
Lum1xi <mrlumixi@gmail.com>
2024-12-20 16:03:53 +0200

    do this i func

--- hw1.py
+++ hw1.py
@@ -1,12 +1,18 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
 
-try:
-    age = int(age)
-    if  age < 0 or age >= 130:
-        raise ValueError("Enter a valid age")
-    else:
-        print(f"Hi {name}Your age is {age}")
 
-except ValueError as e:
-    print(e)
+def print_name_age(name, age):
+    try:
+        age = int(age)
+        if  age < 0 or age >= 130:
+            raise ValueError("Enter a valid age")
+        else:
+            print(f"Hi {name}Your age is {age}")
+
+    except ValueError as e:
+        print(e)
+
+
+InputName = input("Enter your name: ")
+InputAge = input("Enter your age: ")
+
+print_name_age(InputName, InputAge)
